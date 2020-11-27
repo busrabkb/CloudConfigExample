26b496cfe2df165515b04dab8f21b1390f643ab3
xhbkarabulut <xhbkarabulut.havelsan.com.tr>
2020-11-27 15:07:31 +0300

    Initial commit

---
+++ src/main/java/com/serverexample/example/ExampleApplication.java
@@ -0,0 +1,15 @@
+package com.serverexample.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+
+}
